10d20d4ceb0a7d29ce9d21ced88dfbdebb253fdd
kklee0930 <kklee0930@gmail.com>
2023-08-31 09:25:36 +0900

    init: 프로젝트 생성
    프로젝트 생성

---
+++ src/main/java/ssgssak/ssgpointappcoupon/SsgPointAppCouponApplication.java
@@ -0,0 +1,13 @@
+package ssgssak.ssgpointappcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgPointAppCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgPointAppCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssgssak/ssgpointappcoupon/SsgPointAppCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package ssgssak.ssgpointappcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgPointAppCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
