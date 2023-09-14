d79d2060b3896ac9a9a6b2e0f233708720799c66
kklee0930 <kklee0930@gmail.com>
2023-09-14 17:40:15 +0900

    corsconfig 수정

--- src/main/java/ssgssak/ssgpointappcoupon/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointappcoupon/global/config/security/CorsConfig.java
@@ -2,9 +2,11 @@ package ssgssak.ssgpointappcoupon.global.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
